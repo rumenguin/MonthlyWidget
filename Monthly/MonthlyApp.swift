ad602bb478a9ad7189f61c0ff46fb4de8246add1
Rumen Guin <rumen2k@gmail.com>
2022-09-27 21:33:12 +0530

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by RUMEN GUIN on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
